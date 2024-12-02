f2bb18282d51773b66cb71eb2202119632e649f7
JohanPires <johan2408.jp@gmail.com>
2024-12-02 17:06:21 +0100

    profilpage

--- capoeira/src/app/forumpage/forumpage.component.ts
+++ capoeira/src/app/forumpage/forumpage.component.ts
@@ -1,11 +1,76 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-forumpage',
-  imports: [],
+  imports:[CommonModule],
   templateUrl: './forumpage.component.html',
-  styleUrl: './forumpage.component.css'
+  styleUrls: ['./forumpage.component.css']
 })
 export class ForumpageComponent {
+  data = {
+    first: {
+      theme: "Techniques",
+      message: [
+        {
+          user: 'utilisateur 1',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        },
+        {
+          user: 'utilisateur 2',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        },
+        {
+          user: 'utilisateur 3',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        }
+      ]
+    },
+    second: {
+      theme: "Vrille",
+      message: [
+        {
+          user: 'utilisateur 1',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        },
+        {
+          user: 'utilisateur 2',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        },
+        {
+          user: 'utilisateur 3',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        }
+      ]
+    },
+    third: {
+      theme: "Comment débuter",
+      message: [
+        {
+          user: 'utilisateur 1',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        },
+        {
+          user: 'utilisateur 2',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        },
+        {
+          user: 'utilisateur 3',
+          title: 'Comment débuter',
+          text: 'Vous avez des conseils quand on débute dans la capoeira'
+        }
+      ]
+    }
+  };
 
+  // Transformation en tableau pour faciliter l'itération
+  dataArray = Object.values(this.data);
 }
