439d6b4811b318d190de6a19569e857d06e656a0
Sodany Sean <champloo63@gmail.com>
2024-12-02 15:00:18 +0100

    footer added

--- capoeira/src/app/app.component.ts
+++ capoeira/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
