6e5d6e5262ef35da656d91d2e6f0c701e75683c2
JohanPires <johan2408.jp@gmail.com>
2024-12-02 15:21:50 +0100

    nav

--- capoeira/src/app/header/header.component.ts
+++ capoeira/src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
