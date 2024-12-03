de23ab1aecae618b9b86a10c863d7c460664e0a9
JohanPires <johan2408.jp@gmail.com>
2024-12-03 10:17:51 +0100

    services

--- capoeira/src/app/Services/event.service.ts
+++ capoeira/src/app/Services/event.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+export interface Event {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  city: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
+  private url = 'http://localhost:3000/events';
+  private http = inject(HttpClient);
+  private event = signal<Event[]>([])
+
   constructor() { }
+
+  getEvent(): Observable<Event[]> {
+    return this.http.get<Event[]>(this.url).pipe(
+      tap(event => this.event.set(event))
+    );
+  }
 }
